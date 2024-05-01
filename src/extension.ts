2518f8795c84bb601dd90884732039b8c0daf56c
Nikki Schmidt <nikki@nsylv.dev>
2024-05-01 12:35:19 -0500

    feat: update extension to print out selectedText

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,25 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+  const editor = await vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "npm-package-link" is now active!');
+  if (!editor) {
+    return;
+  }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('npm-package-link.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from npm-package-link!');
-	});
+  const selection = editor.selection;
 
-	context.subscriptions.push(disposable);
+  if (!selection) {
+    return;
+  }
+
+  const selectedText = editor.document.getText(selection);
+
+  console.log(selectedText);
 }
 
 // This method is called when your extension is deactivated
